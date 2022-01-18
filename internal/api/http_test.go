5a94101a0229596d21498512153bce1ecb732514
Craig Gumbley <craiggumbley@gmail.com>
2022-01-18 19:42:36 +0000

    Add a new test case for host matching
    The tests added in this commit aim to prove that if the requested
    host does not match opts.Host the auth token is removed from
    the request headers.

--- internal/api/http_test.go
+++ internal/api/http_test.go
@@ -87,6 +87,66 @@ func TestNewHTTPClient(t *testing.T) {
 	}
 }
 
+func TestNewHTTPClientWithDifferentHost(t *testing.T) {
+	reflectHTTP := tripper{
+		roundTrip: func(req *http.Request) (*http.Response, error) {
+			header := req.Header.Clone()
+			body := "{}"
+			return &http.Response{
+				StatusCode: 200,
+				Header:     header,
+				Body:       io.NopCloser(bytes.NewBufferString(body)),
+				Request:    req,
+			}, nil
+		},
+	}
+
+	tests := []struct {
+		name          string
+		headers       map[string]string
+		optsHost      string
+		reqHost       string
+		authToken     string
+		wantAuthToken string
+	}{
+		{
+			name:          "removes authorization header for a different host",
+			optsHost:      "github.com",
+			reqHost:       "https://nothub.com",
+			authToken:     "oauth_token",
+			wantAuthToken: "",
+		},
+		{
+			name:          "does not remove authorization header for a matching host",
+			optsHost:      "github.com",
+			reqHost:       "https://api.github.com",
+			authToken:     "oauth_token",
+			wantAuthToken: "token oauth_token",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := api.ClientOptions{
+				Host:      tt.optsHost,
+				AuthToken: tt.authToken,
+				Headers:   tt.headers,
+				Transport: reflectHTTP,
+			}
+
+			req, err := http.NewRequest(http.MethodGet, tt.reqHost, nil)
+
+			if err != nil {
+				return
+			}
+
+			client := newHTTPClient(&opts)
+			res, err := client.Do(req)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantAuthToken, res.Request.Header.Get("authorization"))
+		})
+	}
+}
+
 type tripper struct {
 	roundTrip func(*http.Request) (*http.Response, error)
 }
