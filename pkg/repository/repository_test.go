7ad1d2c09bea61b2605e8aa77378bb2641c406da
Sam Coe <samcoe@users.noreply.github.com>
2022-05-17 21:30:48 +0200

    Add repository.ParseWithHost (#41)

--- pkg/repository/repository_test.go
+++ pkg/repository/repository_test.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -85,6 +86,110 @@ func TestParse(t *testing.T) {
 				defer os.Setenv("GH_HOST", old)
 			}
 			r, err := Parse(tt.input)
+			if tt.wantErr != "" {
+				assert.EqualError(t, err, tt.wantErr)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantHost, r.Host())
+			assert.Equal(t, tt.wantOwner, r.Owner())
+			assert.Equal(t, tt.wantName, r.Name())
+		})
+	}
+}
+
+func TestParse_hostFromConfig(t *testing.T) {
+	tempDir := t.TempDir()
+	old := os.Getenv("GH_CONFIG_DIR")
+	os.Setenv("GH_CONFIG_DIR", tempDir)
+	defer os.Setenv("GH_CONFIG_DIR", old)
+	var configData = `
+git_protocol: ssh
+editor:
+prompt: enabled
+pager: less
+`
+	var hostData = `
+enterprise.com:
+  user: user2
+  oauth_token: yyyyyyyyyyyyyyyyyyyy
+  git_protocol: https
+`
+	err := os.WriteFile(filepath.Join(tempDir, "config.yml"), []byte(configData), 0644)
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(tempDir, "hosts.yml"), []byte(hostData), 0644)
+	assert.NoError(t, err)
+	r, err := Parse("OWNER/REPO")
+	assert.NoError(t, err)
+	assert.Equal(t, "enterprise.com", r.Host())
+	assert.Equal(t, "OWNER", r.Owner())
+	assert.Equal(t, "REPO", r.Name())
+}
+
+func TestParseWithHost(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		host      string
+		wantOwner string
+		wantName  string
+		wantHost  string
+		wantErr   string
+	}{
+		{
+			name:      "OWNER/REPO combo",
+			input:     "OWNER/REPO",
+			host:      "github.com",
+			wantHost:  "github.com",
+			wantOwner: "OWNER",
+			wantName:  "REPO",
+		},
+		{
+			name:    "too few elements",
+			input:   "OWNER",
+			host:    "github.com",
+			wantErr: `expected the "[HOST/]OWNER/REPO" format, got "OWNER"`,
+		},
+		{
+			name:    "too many elements",
+			input:   "a/b/c/d",
+			host:    "github.com",
+			wantErr: `expected the "[HOST/]OWNER/REPO" format, got "a/b/c/d"`,
+		},
+		{
+			name:    "blank value",
+			input:   "a/",
+			host:    "github.com",
+			wantErr: `expected the "[HOST/]OWNER/REPO" format, got "a/"`,
+		},
+		{
+			name:      "with hostname",
+			input:     "example.org/OWNER/REPO",
+			host:      "github.com",
+			wantHost:  "example.org",
+			wantOwner: "OWNER",
+			wantName:  "REPO",
+		},
+		{
+			name:      "full URL",
+			input:     "https://example.org/OWNER/REPO.git",
+			host:      "github.com",
+			wantHost:  "example.org",
+			wantOwner: "OWNER",
+			wantName:  "REPO",
+		},
+		{
+			name:      "SSH URL",
+			input:     "git@example.org:OWNER/REPO.git",
+			host:      "github.com",
+			wantHost:  "example.org",
+			wantOwner: "OWNER",
+			wantName:  "REPO",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r, err := ParseWithHost(tt.input, tt.host)
 			if tt.wantErr != "" {
 				assert.EqualError(t, err, tt.wantErr)
 				return
