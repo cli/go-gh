faa8afe90ffcf1cd3b3b0c036052cbd9a1d7ac01
Sam Coe <samcoe@users.noreply.github.com>
2022-08-16 10:58:41 +0300

    Migrate jq functionality from cli

--- go.mod
+++ go.mod
@@ -10,12 +10,13 @@ require (
 	github.com/cli/shurcooL-graphql v0.0.1
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/henvic/httpretty v0.0.6
+	github.com/itchyny/gojq v0.12.8
 	github.com/mattn/go-runewidth v0.0.13
 	github.com/microcosm-cc/bluemonday v1.0.19 // indirect
 	github.com/muesli/termenv v0.12.0
 	github.com/stretchr/testify v1.7.0
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 	gopkg.in/h2non/gock.v1 v1.1.2
 	gopkg.in/yaml.v3 v3.0.1
@@ -28,6 +29,7 @@ require (
 	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
+	github.com/itchyny/timefmt-go v0.1.3 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
---
+++ pkg/jq/jq.go
@@ -0,0 +1,93 @@
+// Package jq facilitates processing of JSON strings using jq expressions.
+package jq
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+
+	"github.com/itchyny/gojq"
+)
+
+// Evaluate a jq expression against an input and write it to an output.
+func Evaluate(input io.Reader, output io.Writer, expr string) error {
+	query, err := gojq.Parse(expr)
+	if err != nil {
+		return err
+	}
+
+	code, err := gojq.Compile(
+		query,
+		gojq.WithEnvironLoader(func() []string {
+			return os.Environ()
+		}))
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := io.ReadAll(input)
+	if err != nil {
+		return err
+	}
+
+	var responseData interface{}
+	err = json.Unmarshal(jsonData, &responseData)
+	if err != nil {
+		return err
+	}
+
+	iter := code.Run(responseData)
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if err, isErr := v.(error); isErr {
+			return err
+		}
+		if text, e := jsonScalarToString(v); e == nil {
+			_, err := fmt.Fprintln(output, text)
+			if err != nil {
+				return err
+			}
+		} else {
+			var jsonFragment []byte
+			jsonFragment, err = json.Marshal(v)
+			if err != nil {
+				return err
+			}
+			_, err = output.Write(jsonFragment)
+			if err != nil {
+				return err
+			}
+			_, err = fmt.Fprint(output, "\n")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func jsonScalarToString(input interface{}) (string, error) {
+	switch tt := input.(type) {
+	case string:
+		return tt, nil
+	case float64:
+		if math.Trunc(tt) == tt {
+			return strconv.FormatFloat(tt, 'f', 0, 64), nil
+		} else {
+			return strconv.FormatFloat(tt, 'f', 2, 64), nil
+		}
+	case nil:
+		return "", nil
+	case bool:
+		return fmt.Sprintf("%v", tt), nil
+	default:
+		return "", fmt.Errorf("cannot convert type to string: %v", tt)
+	}
+}
