e8e82da825726cfa002e8782efdd3586bb2e537e
Sam Coe <samcoe@users.noreply.github.com>
2023-02-13 18:27:16 +1100

    Add pagination examples for RESTClient and GQLClient

--- example_gh_test.go
+++ example_gh_test.go
@@ -1,10 +1,13 @@
 package gh_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	gh "github.com/cli/go-gh"
@@ -12,7 +15,6 @@ import (
 	"github.com/cli/go-gh/pkg/tableprinter"
 	"github.com/cli/go-gh/pkg/term"
 	graphql "github.com/cli/shurcooL-graphql"
-	"github.com/shurcooL/githubv4"
 )
 
 // Execute 'gh issue list -R cli/cli', and print the output.
@@ -70,27 +72,64 @@ func ExampleRESTClient_request() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// URL to cli/cli release v2.14.2 checksums.txt
 	assetURL := "repos/cli/cli/releases/assets/71589494"
-	resp, err := client.Request("GET", assetURL, nil)
+	response, err := client.Request(http.MethodGet, assetURL, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-
+	defer response.Body.Close()
 	f, err := os.CreateTemp("", "*_checksums.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
-	_, err = io.Copy(f, resp.Body)
+	_, err = io.Copy(f, response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Printf("Asset downloaded to %s\n", f.Name())
+}
+
+// Get releases from cli/cli repository using REST API with paginated results.
+func ExampleRESTClient_pagination() {
+	var linkRE = regexp.MustCompile(`<([^>]+)>;\s*rel="([^"]+)"`)
+	findNextPage := func(response *http.Response) (string, bool) {
+		for _, m := range linkRE.FindAllStringSubmatch(response.Header.Get("Link"), -1) {
+			if len(m) > 2 && m[2] == "next" {
+				return m[1], true
+			}
+		}
+		return "", false
+	}
+	client, err := gh.RESTClient(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	requestPath := "repos/cli/cli/releases"
+	page := 1
+	hasNextPage := true
+	for hasNextPage {
+		response, err := client.Request(http.MethodGet, requestPath, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := response.Body.Close(); err != nil {
+			log.Fatal(err)
+		}
+		data := []struct{ Name string }{}
+		if err := json.Unmarshal(body, &data); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Page: %d\n", page)
+		fmt.Println(data)
+		requestPath, hasNextPage = findNextPage(response)
+		page++
+	}
 }
 
 // Query tags from cli/cli repository using GQL API.
@@ -155,12 +194,12 @@ func ExampleGQLClient_advanced() {
 }
 
 // Add a star to the cli/go-gh repository using the GQL API.
-func ExampleGQLClient_Mutate_simple() {
+func ExampleGQLClient_mutate_simple() {
 	client, err := gh.GQLClient(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var m struct {
+	var mutation struct {
 		AddStar struct {
 			Starrable struct {
 				Repository struct {
@@ -172,16 +211,58 @@ func ExampleGQLClient_Mutate_simple() {
 			}
 		} `graphql:"addStar(input: $input)"`
 	}
+	type AddStarInput struct {
+		StarrableID string `json:"starrableId"`
+	}
 	variables := map[string]interface{}{
-		"input": githubv4.AddStarInput{
-			StarrableID: githubv4.NewID("R_kgDOF_MgQQ"),
+		"input": AddStarInput{
+			StarrableID: "R_kgDOF_MgQQ",
 		},
 	}
-	err = client.Mutate("AddStar", &m, variables)
+	err = client.Mutate("AddStar", &mutation, variables)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(m.AddStar.Starrable.Repository.StargazerCount)
+	fmt.Println(mutation.AddStar.Starrable.Repository.StargazerCount)
+}
+
+// Query releases from cli/cli repository using GQL API with paginated results.
+func ExampleGQLClient_pagination() {
+	client, err := gh.GQLClient(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var query struct {
+		Repository struct {
+			Releases struct {
+				Nodes []struct {
+					Name string
+				}
+				PageInfo struct {
+					HasNextPage bool
+					EndCursor   string
+				}
+			} `graphql:"releases(first: 30, after: $endCursor)"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
+	}
+	variables := map[string]interface{}{
+		"owner":     graphql.String("cli"),
+		"name":      graphql.String("cli"),
+		"endCursor": (*graphql.String)(nil),
+	}
+	page := 1
+	for {
+		if err := client.Query("RepositoryReleases", &query, variables); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Page: %d\n", page)
+		fmt.Println(query.Repository.Releases.Nodes)
+		if !query.Repository.Releases.PageInfo.HasNextPage {
+			break
+		}
+		variables["endCursor"] = graphql.String(query.Repository.Releases.PageInfo.EndCursor)
+		page++
+	}
 }
 
 // Get repository for the current directory.
